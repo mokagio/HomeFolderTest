84545b1aef9b3c232e0b728f2ea5bee15cac71ec
Gio Lodi <giovanni.lodi42@gmail.com>
2021-04-26 12:07:38 +1000

    Stock Xcode project

---
+++ HomeFolderTest/HomeFolderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeFolderTestApp.swift
+//  HomeFolderTest
+//
+//  Created by Gio on 26/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeFolderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
